e788cbc6060bf77e76e78752a3145ba74849371d
Corina Gum <>
2025-11-03 14:55:35 -0800

    Throw on broken markdown links

--- teams.md/docusaurus.config.ts
+++ teams.md/docusaurus.config.ts
@@ -33,7 +33,7 @@ const config: Config = {
     markdown: {
         mermaid: true,
         hooks: {
-            onBrokenMarkdownLinks: 'warn',
+            onBrokenMarkdownLinks: 'throw',
         },
     },
     headTags: [
